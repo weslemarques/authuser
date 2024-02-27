222422949f42a5ff4adba9d05ed02b9e39176208
weslemarques <wesleremarques@gmail.com>
2024-02-27 00:04:18 -0300

    feature - register user

--- src/main/java/com/ead/authuser/controllers/AuthController.java
+++ src/main/java/com/ead/authuser/controllers/AuthController.java
@@ -1,9 +1,7 @@
 package com.ead.authuser.controllers;
 
 import com.ead.authuser.dtos.UserCreateDTO;
-import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
